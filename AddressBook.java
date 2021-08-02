d8593c501a14bb4fc531ef540755b5e9f0c043a7
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-02 11:32:25 +0530

    Refactored:Add Entries in the Address Book

--- AddressBook.java
+++ AddressBook.java
@@ -1,55 +1,76 @@
 import java.util.Scanner;
 
+public class AddressBook{
+    Scanner S = new Scanner(System.in);
 
-public class AddressBook {
-	      
-	  
-	private String firstName;
-    private String lastName;
-	private String address;
-	private String city;
-	private String state;
-	private int zipCode;
-	private long phoneNumber;
-	private String email;
-	      
+    //Nested class
+    class Entries{
+        private String firstName;
+        private String lastName;
+        private String address;
+        private String city;
+        private String state;
+        private String zipCode;
+        private String phoneNumber;
+        private String email;
 
-	void entries() {
-        Scanner S = new Scanner(System.in);
+        Entries(String firstName, String lastName, String address, String city,
+                String state, String zipCode, String phoneNumber, String email){
 
-	    System.out.print("\nFirst Name : ");
-	    firstName = S.nextLine();
-	    System.out.print("\nLast Name : ");
-	    lastName = S.nextLine();
-	    System.out.print("\nComplete Address : ");
-	    address = S.nextLine();
-	    System.out.print("\nCity : ");
-	    city  = S.nextLine();
-	    System.out.print("\nState : ");
-	    state  = S.nextLine(); 
-	    System.out.println("\nE-mail Address : ");
-	    email = S.nextLine();
-	    System.out.print("\nZip Code : ");
-	    zipCode = S.nextInt();
-	    System.out.print("\nPhone Number : ");
-	    phoneNumber = S.nextLong();
-	}
-	    public void readEntries() {
-	    System.out.println("First Name:"+firstName );
-	    System.out.println("Last Name:"+lastName );
-	    System.out.println("Complete Address:"+address );
-	    System.out.println("City:"+city );
-	    System.out.println("State:"+state);
-	    System.out.println("E-mail Address:"+email );
-	    System.out.println("Zip Code:"+zipCode );
-	    System.out.println("Phone Number:"+phoneNumber );
-	}
-    public static void main(String[] args) {
-         	   
-        AddressBook ab = new AddressBook();
-         	   
-        ab.entries();
-        ab.readEntries();
-         	   
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city=city;
+            this.state=state;
+            this.zipCode=zipCode;
+            this.phoneNumber=phoneNumber;
+            this.email = email;
+        }
+        Entries(){
+            firstName = "";
+            lastName = "";
+            address = "";
+            city = "";
+            state = "";
+            zipCode= "";
+            phoneNumber = "";
+            email = "";
+        }
+
+        public void readEntries(){
+            System.out.println("First Name:"+firstName );
+            System.out.println("Last Name:"+lastName );
+            System.out.println("Complete Address:"+address );
+            System.out.println("City:"+city );
+            System.out.println("State:"+state);
+            System.out.println("Zip Code:"+zipCode );
+            System.out.println("Phone Number:"+phoneNumber );
+            System.out.println("E-mail:"+email );
+        }
+    }
+
+    // Check How Many Files in Address Book
+    private int entries = 0;
+    Entries[] c;
+    public void initEntry(int b){
+        c = new Entries[b];
+        for (int i = 0;i<c.length;i++){    
+            c[i] = new Entries();
+        }
+    }
+    public int getEntry(){
+        return c.length;
+    }
+    //Adds Entry to the Address Book
+    public void add(String firstName, String lastName, String address, String city, 
+                        String state, String zip,String phoneNumber, String email){
+
+        if (entries<c.length){
+
+        c[entries] = new Entries (firstName, lastName, address, city, 
+                                        state, zip, phoneNumber, email);
+        entries++;
+        }
+        else System.out.println("Error : Address Book is Already Full!");
     }
 }
---
+++ AddressBookMain.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+
+public class AddressBookMain {
+	      
+	  
+	public static void main(String[] args){
+
+	    Scanner S = new Scanner(System.in);
+	    System.out.print("How many Address Books you want to Create? : ");
+	    int maxEntries;
+	    int howManyEntries;
+
+	    AddressBook[] ab = new AddressBook[0];
+
+
+	    while(true){
+        // Enter maximum entries in address book
+	        maxEntries = S.nextInt();
+	        if (maxEntries>0){
+	            ab = new AddressBook[maxEntries];
+	            break;
+	        }
+	        else {
+                System.out.print("You must create at least 1 entry.");
+	        }
+		}
+
+        for (int i=0;i<ab.length;i++){
+            ab[i] = new AddressBook(); 
+
+	        while(true){
+	            System.out.print("How many Entries you want to create in " +i+ " Address Book");
+	            howManyEntries = S.nextInt();
+	            if (howManyEntries>0) {
+	                ab[i].initEntry(howManyEntries);
+	                break;
+	            }
+	            else {
+				    System.out.println("You must create at least 1 Entry.");
+	            }
+            }
+	    }
+		boolean done = false;
+	    int bookSelected = 0;
+	    int select;
+	    while (done==false){
+	        System.out.println("Address Book "+bookSelected+" is Selected Currently.");
+
+	        for (int j = 0; j < ab[bookSelected].getEntry(); j++){
+	            System.out.println("===========Entry "+j+" ===========");
+	            ab[bookSelected].c[j].readEntries();
+	            System.out.println("================================");
+
+	        }
+			System.out.println("Select an Option!");
+	        System.out.println("1. Add an Entry");
+	        System.out.println("2. Exit the Menu");
+	        System.out.print("> ");
+	        select = S.nextInt();
+	        String firstName, lastName, address, city, state, zipCode, phoneNumber, email;
+	        switch(select){
+	            case 1: 
+	                System.out.print("First name? ");
+	                firstName = S.next();
+	                System.out.print("Last name? ");
+	                lastName = S.next();
+	                System.out.print("Complete Address? ");
+	                address = S.next();
+	                System.out.print("City? ");
+	                city = S.next();
+					System.out.println("State? ");
+					state = S.next();
+	                System.out.print("Zip Code? ");
+	                zipCode = S.next();
+	                System.out.print("Phone Number? ");
+	                phoneNumber = S.next();
+	                System.out.print("E-mail? ");
+	                email = S.next();
+	                ab[bookSelected].add(firstName, lastName, address, city, 
+					                            state, zipCode,phoneNumber, email);
+	                break;
+	                
+
+	            case 2:
+	                done = true;
+	                break;
+	            default:
+	                System.out.print("Kindly Select a Valid Menu Number!");
+
+			}
+		}
+    }
+}
